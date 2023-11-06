1c1f767ff83853c82c72ed3d54aeb3304fb42f5a
Md.Sarwar Alam <sarwaralam.ini@gmail.com>
2023-11-07 00:48:26 +0600

    Some Basic Setup

---
+++ inc/header.php
@@ -0,0 +1,16 @@
+<header class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="header-left">
+
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="header-right">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Bludo - Multipurpose eCommerce HTML Template</title>
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="" />
+        <meta property="og:type" content="" />
+        <meta property="og:url" content="" />
+        <meta property="og:image" content="" />
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="assets/imgs/theme/favicon.svg" />
+        <!-- Template CSS -->
+        <link rel="stylesheet" href="assets/css/style.css" />
+    </head>
+    <body>
+    <?php
+        include_once('inc/header.php');
+    ?>
+    <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-migrate-3.4.1.js" integrity="sha256-CfQXwuZDtzbBnpa5nhZmga8QAumxkrhOToWweU52T38=" crossorigin="anonymous"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/app.js"></script>
+    </body>
+</html>
